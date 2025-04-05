7dfd6483ce1d63de0ea37f0cbf3a8c2420a4bb33
Joao Milone <joaocmil@hotmail.com>
2025-04-04 23:54:44 -0300

    setup complete

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "8067125547:AAHZyargTg1eMtnkFe7Nynbb2fVO-BkEUZI"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "6563608302"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
